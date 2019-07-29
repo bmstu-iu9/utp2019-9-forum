9cc55026f17a98be63160df5cbbe6ff0cf4cd431
unknown <datnguyen79198@gmail.com>
2019-07-29 09:48:03 +0300

    #17 alert wrong user/password when user login

--- scripts/loginScripts.js
+++ scripts/loginScripts.js
@@ -1,5 +1,27 @@
+var xmlHttp = new XMLHttpRequest();
+var myDB;
+
+xmlHttp.onreadystatechange =function() {
+    if (this.readyState==4 && this.status==200) {
+       myDB = JSON.parse(this.responseText);
+    }
+};
+
+xmlHttp.open("GET", "/users");
+xmlHttp.send();
+
 var exceptionHandle = (form) => {
     var pwd = form.password.value;
-    alert(pwd);
-    if (pwd !="dat") return false;
+    var index = -1;
+    for (var i=0;i<Object.keys(myDB.Users).length;i++)
+        if (myDB.Users[i].username == form.username.value) index = i;
+    if (index == -1) {
+        alert('No such this user');
+        return false;
+    }
+    else if (pwd != myDB.Users[index].password) {
+        alert("Password isn't correct");
+        return false;
+    }
+
 }
